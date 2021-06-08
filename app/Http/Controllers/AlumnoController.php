30744789d0977e3c727c233c892ad6c762b0807b
Denis Espinoza <denisjev@gmail.com>
2021-06-08 16:46:16 -0600

    Add AlumnoController

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    public function show() {
+        return "Listado de alumnos";
+    }
+}
